9ae7f9d6f8b4b8917c2fd59761c507bd0f15af2e
幻想不要钱 <huijia0211@gmail.com>
2020-01-08 16:14:53 +0800

    原型模式

---
+++ src/main/java/com/huijia/creational/prototype/abstractprototype/B.java
@@ -0,0 +1,8 @@
+package com.huijia.creational.prototype.abstractprototype;
+
+public class B extends A {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        B b = new B();
+        B clone = (B)b.clone();
+    }
+}
